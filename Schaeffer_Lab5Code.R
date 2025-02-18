d962b67a1f4afb5571e97a0f26487da51783bc4c
Jack Schaeffer <jschaeffer@colgate.edu>
2025-02-18 14:30:19 -0500

    Step 1 Part 2
    Completed Step 1 for overall loudness, now need to alter code to work in a for loop

--- Schaeffer_Lab5Code.R
+++ Schaeffer_Lab5Code.R
@@ -8,19 +8,35 @@ library(jsonlite)
 
 ###Loading data files
 artist.data = read.csv("data/essentia.data.csv")
-allentown.data = read.csv("data/essentia.data.allentown.csv")
+allentown = read.csv("data/essentia.data.allentown.csv")
 IQR(grouped.data$overall_loudness)
 
 ###Grouping data by artist and summarizing
 grouped.data = artist.data|>
-  group_by(artist) |>
+  group_by(artist) |> #Moving on to summarize general qualities
   summarize(min = min(overall_loudness),
             Q1 = quantile(overall_loudness, 0.25),
             Q3 = quantile(overall_loudness, 0.75),
             LF = Q1 - 1.5*IQR(overall_loudness),
             UF = Q3 + 1.5*IQR(overall_loudness),
             max = max(overall_loudness)
-            )
+            ) |> 
+  #Adding new column to check if Allentown is within bounds
+  mutate(out.of.range = (allentown$overall_loudness > max | 
+                           allentown$overall_loudness < min)) |>
+  mutate(unusual = (allentown$overall_loudness > UF |
+                      allentown$overall_loudness < LF)) |>
+  #Adding a new column to describe properties of where Allentown lies in bounds
+  mutate(description = case_when(out.of.range == TRUE ~ "Out of Range",
+                                               unusual == TRUE ~ "Outlying",
+                                               TRUE       ~ "Within Range"))
+
+
+
+
+
+view(grouped.data)
+view(allentown)
 
 
 
