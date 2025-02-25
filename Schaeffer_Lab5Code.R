957d1a7dc29fc43aad5613370d1ab58dfabf2633
Jack Schaeffer <jschaeffer@colgate.edu>
2025-02-24 23:50:09 -0500

    Organized/Grouped Data

--- Schaeffer_Lab5Code.R
+++ Schaeffer_Lab5Code.R
@@ -7,37 +7,60 @@ library(tidyverse)
 library(jsonlite)
 
 ###Loading data files
-artist.data = read.csv("data/essentia.data.csv")
+artists = read.csv("data/essentia.data.csv")
 allentown = read.csv("data/essentia.data.allentown.csv")
-IQR(grouped.data$overall_loudness)
 
-###Grouping data by artist and summarizing
-grouped.data = artist.data|>
-  group_by(artist) |> #Moving on to summarize general qualities
-  summarize(min = min(overall_loudness),
-            Q1 = quantile(overall_loudness, 0.25),
-            Q3 = quantile(overall_loudness, 0.75),
-            LF = Q1 - 1.5*IQR(overall_loudness),
-            UF = Q3 + 1.5*IQR(overall_loudness),
-            max = max(overall_loudness)
-            ) |> 
-  #Adding new column to check if Allentown is within bounds
-  mutate(out.of.range = (allentown$overall_loudness > max | 
-                           allentown$overall_loudness < min)) |>
-  mutate(unusual = (allentown$overall_loudness > UF |
-                      allentown$overall_loudness < LF)) |>
-  #Adding a new column to describe properties of where Allentown lies in bounds
-  mutate(description = case_when(out.of.range == TRUE ~ "Out of Range",
-                                               unusual == TRUE ~ "Outlying",
-                                               TRUE       ~ "Within Range"))
+###Making a vector of all column names
+columns <- artists %>% select(where(is.numeric)) %>% colnames()
+
+### FOR LOOP TO SUMMARIZE EVERY PARAMETER ###
+#Grouping together our dataframe
 
 
+final_df = data.frame() #Making an empty df to store all data
+
+for (feature in columns){
+  
+  ###Grouping data by artist and summarizing
+  grouped = artists|>
+    group_by(artist) |> #Moving on to summarize general qualities
+    summarize(min = min(get(feature)),
+              Q1 = quantile(get(feature), 0.25, na.rm = TRUE),
+              Q3 = quantile(get(feature), 0.75, na.rm = TRUE),
+              LF = Q1 - 1.5*IQR(get(feature), na.rm = TRUE),
+              UF = Q3 + 1.5*IQR(get(feature), na.rm = TRUE),
+              max = max(get(feature))
+    ) |> 
+    
+    #Adding new column to check if Allentown is within bounds
+    mutate(out.of.range = (get(feature, allentown) > max | 
+                             get(feature, allentown) < min)) |>
+    mutate(unusual = (get(feature, allentown) > UF |
+                        get(feature, allentown) < LF)) |>
+    
+    #Adding a new column to describe properties of where Allentown lies in bounds
+    mutate(description = case_when(out.of.range == TRUE ~ "Out of Range",
+                                   unusual == TRUE ~ "Outlying",
+                                   TRUE       ~ "Within Range")) |>
+    mutate(feature = feature)
+  
+  final_df = rbind(final_df, grouped) #Combining the new feature into final dataframe
+  
+}
+### Reorganizing dataframe - making feature second and description third
+final_df <- final_df %>%
+  select(artist, feature, description, everything())
+
+############################################################################
+##########              STEP TWO: Analyzing the Data              ##########
+############################################################################
 
 
 
-view(grouped.data)
-view(allentown)
 
+
+view(artists)
+view(final_df)
 
 
 
